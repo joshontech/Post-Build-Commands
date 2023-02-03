644d20a7bb9fd8d86e137ba45e434f32c9827a53
joshontech <44295541+joshontech@users.noreply.github.com>
2023-02-03 11:32:13 -0500

    Clear Screen Added to Windows Update .bat

--- Windows-Update.bat
+++ Windows-Update.bat
@@ -1,12 +1,15 @@
 @echo off
 
+echo.
+echo.
 echo Starting Script.
+cls
 
 powershell -command "Install-PackageProvider -Name NuGet -Force"
-
+cls
 
 powershell -command "Install-Module -Name PSWindowsUpdate -Force"
-
+cls
 
 powershell -command "Import-Module -Name PSWindowsUpdate -Force"
 cls
