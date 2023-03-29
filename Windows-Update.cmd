c4e7bbf9bf4b872b084b3857f765df7f3a5c6dfa
joshontech <44295541+joshontech@users.noreply.github.com>
2023-03-29 13:32:55 -0400

    Possibly Fixed some things in Windows-Update

--- Windows-Update.cmd
+++ Windows-Update.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 :: BatchGotAdmin
-:-------------------------------------
+:----------------------------------------------------------------
 :: Check if the script is running with administrator privileges
 net session >nul 2>&1
 if %errorLevel% == 0 (
@@ -19,7 +19,7 @@ exit /B
 :gotAdmin
 :: Continue with the script's main logic
 pushd "%~dp0"
-:--------------------------------------
+:----------------------------------------------------------------
 
 PowerShell -command "if((Get-ExecutionPolicy ) -ne 'Unrestricted') {exit 1}"
 
@@ -32,27 +32,23 @@ if %errorlevel%==1 (
 
 cls
 
-
-echo.
 echo.
 echo Updates will start momentarilly...
-PowerShell -command "Install-PackageProvider -Name NuGet -Force" -WindowStyle Hidden > nul
-PowerShell -command "Install-Module -Name PSWindowsUpdate -Force" -WindowStyle Hidden > nul
-PowerShell -command "Import-Module -Name PSWindowsUpdate -Force" -WindowStyle Hidden > nul
-    
-
+PowerShell -command "Install-PackageProvider -Name NuGet -Force" > nul 2>&1
+PowerShell -command "Install-Module -Name PSWindowsUpdate -Force" > nul 2>&1
+PowerShell -command "Import-Module -Name PSWindowsUpdate -Force" > nul 2>&1
 cls
 
 PowerShell -command "Get-WindowsUpdate -Install -AcceptAll -Verbose -IgnoreReboot"
 
-
-REM Checking if Windows needs to be rebooted.
-reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" >nul 2>&1
+:: Checking if Windows needs to be rebooted.
+reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" > nul 2>&1
 if %errorlevel% equ 0 (
     exit /b 1
 )
 
-reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\PendingFileRenameOperations" >nul 2>&1
+:: Checking if Windows needs to be rebooted.
+reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\PendingFileRenameOperations" > nul 2>&1
 if %errorlevel% equ 0 (
     exit /b 1
 )
@@ -60,13 +56,14 @@ if %errorlevel% equ 0 (
 if %errorlevel% equ 0 (
   set RebootRequired=1
 ) else (
-  call :PowerShell-ExecutionPolicy-Check
-  echo.
-  echo.
-  echo No reboot is required.
-  pause
-  call :DeleteNuGet
-  exit /b
+    call :PowerShell-ExecutionPolicy-Check
+    call :UninstallModule
+    call :DeleteNuGet
+    echo.
+    echo.
+    echo No reboot is required.
+    pause
+    exit /b
 )
 
 if %RebootRequired% equ 1 (
@@ -77,22 +74,26 @@ if %RebootRequired% equ 1 (
 )
 if /i "%choice%" equ "Y" (
   call :PowerShell-ExecutionPolicy-Check
+  call :UninstallModule
   call :DeleteNuGet
   shutdown /r /t 0
   exit /b
   
-)   else (
-      call :PowerShell-ExecutionPolicy-Check
-      echo.
-      echo.
-      echo You may restart Windows at a later time.
-      pause     
-      call :DeleteNuGet
-      exit /b
+) else (
+    call :PowerShell-ExecutionPolicy-Check
+    call :UninstallModule
+    call :DeleteNuGet
+    echo.
+    echo.
+    echo You may restart Windows at a later time.
+    pause
+    exit /b
 ) 
 
-:PowerShell-ExecutionPolicy-Check
-if %PowerShell-Enabled-At-Start% equ 1 (
+:: Functions
+:---------------------------------------------------------------------------
+:PowerShell-ExecutionPolicy-Check                                          
+  if %PowerShell-Enabled-At-Start% equ 1 (
     call
 ) else (
     PowerShell -Command "Set-ExecutionPolicy Restricted -Force"
@@ -100,9 +101,13 @@ if %PowerShell-Enabled-At-Start% equ 1 (
 exit /b
 
 :DeleteNuget
-  cd C:\Program Files\
-  if exist PackageManagement (
-    rmdir /s /q PackageManagement
- )   else (
-      call
- )
+  cd C:\Program Files\PackageManagement\ProviderAssemblies
+  if exist nuget (
+     rmdir /s /q nuget
+     exit /b
+  )
+
+:UninstallModule
+  PowerShell -command "Uninstall-Module -Name PSWindowsUpdate -Force" > nul 2>&1
+  exit /b
+:----------------------------------------------------------------------------
